340c409068adad462ed09755121afaace7ae045a
Jettapol Tuetrakul <14010776+buumu8@users.noreply.github.com>
2025-10-28 02:34:12 +0900

    Updates

---
+++ assets/first-post-CNBr69Oa.js
@@ -0,0 +1,23 @@
+const t=`---
+title: "My First Post"
+date: "2025-10-27"
+tags: ["React", "Portfolio"]
+image: "\${import.meta.env.BASE_URL}images/first-post.jpg"
+---
+
+This is the **content of my first blog post**.
+
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+Test
+
+`;export{t as default};
---
+++ assets/second-post-BSRM8DxM.js
@@ -0,0 +1,10 @@
+const t=`---
+title: "My Second Post"
+date: "2025-10-27"
+tags: ["React", "Portfolio"]
+---
+
+This is the **content of my second blog post**.
+
+Test Test
+`;export{t as default};
